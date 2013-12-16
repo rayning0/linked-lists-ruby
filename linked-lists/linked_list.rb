f755cf234d7b61ff1532523317d08ab219b0a1d0
Raymond Gan <rayning@yahoo.com>
2013-12-16 11:15:17 -0500

    Finished deque (double-ended queue) exercise, using Forwardable module and def_delegators

--- linked-lists/linked_list.rb
+++ linked-lists/linked_list.rb
@@ -1,1 +1,12 @@
-linked_list.rb
+require 'forwardable'
+
+class Deque
+  attr_accessor :queue
+
+   extend Forwardable
+   def_delegators :@queue, :push, :pop, :unshift, :shift
+
+   def initialize
+     self.queue = []
+   end
+end
